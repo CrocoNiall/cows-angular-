e1751847e7d776c123fa3b1d0fcdbb7d646c317f
Niall Wallace <nj.wallace@outlook.com>
2015-10-18 17:55:33 +0100

    styled frontend

--- api/app.js
+++ api/app.js
@@ -29,9 +29,9 @@ app.use(routes);
 
 // QUESTION 1
 var post1 = new Post({
-  userName: 'Niallio',
-  postTitle: 'TestPost',
-  postText: 'This is very interesting post'
+  userName: 'Harry The Cow',
+  postTitle: 'Omero undead survivor',
+  postText: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.'
 })
 
 post1.save(function(err, post) {
